89d4c742a6a7de20a486e7b2adcd821aec76f454
ciazhar <ciazhar.id@gmail.com>
2018-01-06 01:08:30 +0700

    fix error env

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-require('dotenv')
+require('dotenv').config()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
